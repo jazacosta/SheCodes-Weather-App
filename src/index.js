d45d7960d2ff2eac2f0b1023e4138c9741852d30
jazacosta <jazmin.acosta.lisboa@gmail.com>
2024-10-07 20:16:24 -0300

    Added the search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function handleSearchSubmit (event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-input");
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#current-city");
 
-    cityElement.innerHTML = searchInput.value;
-    
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+    let apiKey = "04b441f44d2aca3f825t4affa3587e0o";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-input");
+
+    searchCity(searchInput.value)
 }
 
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Asuncion");
